6f56befabbd54557eabacc993b13143d31278782
Subha <subhajeet37@gmail.com>
2023-10-10 19:31:04 +0530

    added some important points on nested element

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 
-
+// creation of nested element
 const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, 
 [React.createElement("h1", {}, "Hi i am h1 tag!"), React.createElement("h1", {}, "Hi i am h1 tag!")]
 ));
 
-
-
-
-
-
-
+// creation of a single element
 const heading = React.createElement("h1", {id: 'parent'}, "Hello World using React!");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
