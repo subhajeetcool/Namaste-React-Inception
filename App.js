d11b2f9835f47084710f986f5aaaaf9954a34ed6
Subha <subhajeet37@gmail.com>
2023-10-11 08:23:32 +0530

    updated few multiple elements

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 
 // creation of nested element
-const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, 
-[React.createElement("h1", {}, "Hi i am h1 tag!"), React.createElement("h1", {}, "Hi i am h1 tag!")]
-));
+const parent = React.createElement("div", {id: "parent"}, 
+React.createElement("div", {id: "child"}, 
+[React.createElement("h1", {}, "Hi i am h1 tag!"), React.createElement("h1", {}, "Hi i am h2 tag!")]
+))
+
+
 
 // creation of a single element
 const heading = React.createElement("h1", {id: 'parent'}, "Hello World using React!");
